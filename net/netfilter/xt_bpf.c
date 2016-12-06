cde1289f9d41e1aa480e6305440d6998f688cbf3
Willem de Bruijn <willemb@google.com>
2016-12-06 16:25:02 -0500

    UPSTREAM: netfilter: xt_bpf: support ebpf
    Add support for attaching an eBPF object by file descriptor.
    
    The iptables binary can be called with a path to an elf object or a
    pinned bpf object. Also pass the mode and path to the kernel to be
    able to return it later for iptables dump and save.
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Chenbo Feng <fengc@google.com>
    (cherry picked from commit 2c16d60332643e90d4fa244f4a706c454b8c7569)
    
    Change-Id: I31b8831a7ffd7c44985ee906ff194c1d934dafbe
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- net/netfilter/xt_bpf.c
+++ net/netfilter/xt_bpf.c
@@ -1,78 +1,40 @@
-/* Xtables module to match packets using a BPF filter.
- * Copyright 2013 Google Inc.
- * Written by Willem de Bruijn <willemb@google.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- */
+#ifndef _XT_BPF_H
+#define _XT_BPF_H
 
-#include <linux/module.h>
-#include <linux/skbuff.h>
 #include <linux/filter.h>
+#include <linux/limits.h>
+#include <linux/types.h>
 
-#include <linux/netfilter/xt_bpf.h>
-#include <linux/netfilter/x_tables.h>
+#define XT_BPF_MAX_NUM_INSTR	64
+#define XT_BPF_PATH_MAX		(XT_BPF_MAX_NUM_INSTR * sizeof(struct sock_filter))
 
-MODULE_AUTHOR("Willem de Bruijn <willemb@google.com>");
-MODULE_DESCRIPTION("Xtables: BPF filter match");
-MODULE_LICENSE("GPL");
-MODULE_ALIAS("ipt_bpf");
-MODULE_ALIAS("ip6t_bpf");
+struct bpf_prog;
 
-static int bpf_mt_check(const struct xt_mtchk_param *par)
-{
-	struct xt_bpf_info *info = par->matchinfo;
-	struct sock_fprog_kern program;
+struct xt_bpf_info {
+	__u16 bpf_program_num_elem;
+	struct sock_filter bpf_program[XT_BPF_MAX_NUM_INSTR];
 
-	if (info->bpf_program_num_elem > XT_BPF_MAX_NUM_INSTR)
-		return -EINVAL;
-
-	program.len = info->bpf_program_num_elem;
-	program.filter = info->bpf_program;
-
-	if (bpf_prog_create(&info->filter, &program)) {
-		pr_info("bpf: check failed: parse error\n");
-		return -EINVAL;
-	}
-
-	return 0;
-}
-
-static bool bpf_mt(const struct sk_buff *skb, struct xt_action_param *par)
-{
-	const struct xt_bpf_info *info = par->matchinfo;
-
-	return BPF_PROG_RUN(info->filter, skb);
-}
-
-static void bpf_mt_destroy(const struct xt_mtdtor_param *par)
-{
-	const struct xt_bpf_info *info = par->matchinfo;
-	bpf_prog_destroy(info->filter);
-}
-
-static struct xt_match bpf_mt_reg __read_mostly = {
-	.name		= "bpf",
-	.revision	= 0,
-	.family		= NFPROTO_UNSPEC,
-	.checkentry	= bpf_mt_check,
-	.match		= bpf_mt,
-	.destroy	= bpf_mt_destroy,
-	.matchsize	= sizeof(struct xt_bpf_info),
-	.usersize	= offsetof(struct xt_bpf_info, filter),
-	.me		= THIS_MODULE,
+	/* only used in the kernel */
+	struct bpf_prog *filter __attribute__((aligned(8)));
 };
 
-static int __init bpf_mt_init(void)
-{
-	return xt_register_match(&bpf_mt_reg);
-}
+enum xt_bpf_modes {
+	XT_BPF_MODE_BYTECODE,
+	XT_BPF_MODE_FD_PINNED,
+	XT_BPF_MODE_FD_ELF,
+};
 
-static void __exit bpf_mt_exit(void)
-{
-	xt_unregister_match(&bpf_mt_reg);
-}
+struct xt_bpf_info_v1 {
+	__u16 mode;
+	__u16 bpf_program_num_elem;
+	__s32 fd;
+	union {
+		struct sock_filter bpf_program[XT_BPF_MAX_NUM_INSTR];
+		char path[XT_BPF_PATH_MAX];
+	};
 
-module_init(bpf_mt_init);
-module_exit(bpf_mt_exit);
+	/* only used in the kernel */
+	struct bpf_prog *filter __attribute__((aligned(8)));
+};
+
+#endif /*_XT_BPF_H */
